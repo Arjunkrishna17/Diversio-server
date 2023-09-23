4fa85e8bfdc503a5ac83917d80c5c52811f6d44e
Krishna-Arjun <krishna.arjun@praximax.com>
2023-09-23 19:05:04 +0530

    MF-108 API to fetch product details with product ids

---
+++ src/Controllers/Cart.ts
@@ -0,0 +1,23 @@
+import { RequestHandler } from "express";
+import Product from "../Modals/Product";
+import { Logger } from "../Utils/Logger";
+
+const Cart: RequestHandler = async (req, res, next) => {
+  try {
+    const productIds = req.query.productIds as string;
+
+    if (productIds) {
+      const ids = productIds.split(",");
+      const products = await Product.find({ _id: { $in: ids } });
+
+      res.status(200).json(products);
+    } else {
+      res.status(404).json("No items added yet");
+    }
+  } catch (error) {
+    Logger.error("Failed to fetch products in the cart: " + error);
+    res.status(404).json("Failed to get products, please try again later.");
+  }
+};
+
+export default Cart;
